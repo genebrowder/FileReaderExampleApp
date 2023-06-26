4e075d525d64d8e0751e3f59dff741598e513d54
Gene Browder <genebrowder@gmail.com>
2023-06-26 05:40:41 -0500

    First commit

---
+++ src/com/coderscampus/FileReaderApp.java
@@ -0,0 +1,15 @@
+package com.coderscampus;
+import com.coderscampus.service.FileService;
+
+public class FileReaderApp {
+
+	public FileReaderApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		FileService fileService = FileService.getInstance();
+
+	}
+
+}
---
+++ src/com/coderscampus/service/FileService.java
@@ -0,0 +1,18 @@
+package com.coderscampus.service;
+
+public class FileService {
+	
+	private static FileService fileService = null;
+	
+	private FileService() {
+		
+	}
+	
+	public static FileService getInstance() {
+		if (fileService == null) {
+			fileService = new FileService();
+		}
+		return fileService;
+	}
+
+}
